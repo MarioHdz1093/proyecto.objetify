5554de38e5638427b936472c1a15358d4ead2f14
Mario Hernandez <gansel1093@gmail.com>
2017-08-31 11:10:05 -0600

    vv

--- biblioteca.java
+++ biblioteca.java
@@ -1,65 +1,24 @@
 import java.util.ArrayList;
 
+
 public class biblioteca
 {
-    private arrayList<String> canciones;  
+    private ArrayList<String> canciones;  
+    private Reproductor reproductor;
     
-    public biblioteca(int claveIni, String nombreIni)
+    public biblioteca()
     {
-        nombre = nombreIni;
-        
+        canciones = new ArrayList<String>();
+        reproductor = new Reproductor();
     }
     
-    public int buscaCancion(String nombreCancion)
+    public void agregarCancion(String nombreArchivo)
     {
-        for(int i=0; i < biblioteca.length; i++)
-        {
-            if( nombreCancion == biblioteca[i].dimeNombre())
-            {
-                return biblioteca[i].dimeNombre();
-            }
-        }
-        return -1;
+        canciones.add(nombreArchivo);
     }
     
-    public boolean inscrbir(cancion unaCancion)
+    public void reproducir(int numeroCancion)
     {
-        int existe = this.buscaCancion(unaCancion.dimeDetalles());
-        if (existe == 1)
-        {
-            return false; //cancion ya existe
-        }    
-        
-        int posDisponible = this.buscaEspacioDisponible();
-        if (posDisponible != -1)
-        {
-            return false;
-        }
-        biblioteca[posDisponible] = unaCancion;
-        
-        return true;
-    }
-    
-    private int buscaEspacioDisponible()
-    {
-        for(int i=0; i < estudiantes.length; i++)
-        {
-            if([i] == null)
-            {
-                return 1;
-            }
-        }
-        return -1;
-    }
-    
-    public void borrarCancion(int clave)
-    {
-        for(int i=0; i < estudiantes.length; i++)
-        {
-            if( clave == estudiantes[i].dimeClave())
-            {
-                estudiantes[i] = null;
-            }
-        }
+        reproductor.reproducir(canciones.get(numeroCancion) );
     }
 }
